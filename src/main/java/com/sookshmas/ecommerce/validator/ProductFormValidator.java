30040939d405da51c3953dd609b4ab23de3ed2e6
sharan9678 <sharan9678@gmail.com>
2022-08-28 21:02:37 +0530

    added validator for customer and product form

---
+++ src/main/java/com/sookshmas/ecommerce/validator/ProductFormValidator.java
@@ -0,0 +1,35 @@
+package com.sookshmas.ecommerce.validator;
+
+import com.sookshmas.ecommerce.entity.Product;
+import com.sookshmas.ecommerce.form.ProductForm;
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.Validator;
+
+public class ProductFormValidator implements Validator {
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return clazz == ProductForm.class;
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        ProductForm productForm = (ProductForm) target;
+
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "code", "NotEmpty.productForm.code");
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "name", "NotEmpty.productForm.name");
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "price", "NotEmpty.productForm.price");
+
+        String code = productForm.getCode();
+        if (code != null && code.length() > 0) {
+            if (code.matches("\\s+")) {
+                errors.rejectValue("code", "Pattern.productForm.code");
+            } else if (productForm.isNewProduct()) {
+                Product product = productDAO.findProduct(code);
+                if (product != null) {
+                    errors.rejectValue("code", "Duplicate.productForm.code");
+                }
+            }
+        }
+    }
+}
