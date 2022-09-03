9a9387fceed4de9855e88b80a7f5db2680984d76
sharan9678 <sharan9678@gmail.com>
2022-09-03 20:31:17 +0530

    added initial frontend

---
+++ src/main/frontent/ecommerce/src/header.js
@@ -0,0 +1,11 @@
+function Header() {
+    return (
+      <div className="Header">
+            <p>
+                this is a dummy text
+            </p>
+      </div>
+    );
+  }
+  
+  export default Header;
